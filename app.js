0a62ef1c65658926c28d31036f44f9654b4c6742
DaviFalcao-prog <davifalcao1609@outlook.com>
2025-06-05 16:22:55 -0300

    Centralizando Section selecionada e destacando

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const navLinks = document.querySelectorAll('.navbar a');
+const allSections = document.querySelectorAll('section');
 
 navLinks.forEach(function (link) {
   link.addEventListener('click', function (e) {
@@ -6,12 +7,17 @@ navLinks.forEach(function (link) {
 
     const id = this.getAttribute('href'); 
     const section = document.querySelector(id);
-    if (!section) return;                  
+    if (!section) return;
 
+    //centraliza a seção na tela
     const y =
       section.offsetTop -
       (window.innerHeight / 2 - section.offsetHeight / 2);
 
-    window.scrollTo({ top: y, behavior: 'smooth' });
+    window.scrollTo({ top: y});
+
+    //Animação de Destaque das seções, achei que ficou legal então vou deixar comentado aqui.
+    //section.classList.add('flash');
+    //setTimeout(() => section.classList.remove('flash'), 1000);
   });
 });
