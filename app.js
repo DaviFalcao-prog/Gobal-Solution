c657a98545397bf29d52e077f12bcfa9821c5f63
DaviFalcao-prog <davifalcao1609@outlook.com>
2025-06-05 23:59:00 -0300

    Configurando menu hamburguer

--- app.js
+++ app.js
@@ -23,3 +23,21 @@ navLinks.forEach(function (link) {
     //setTimeout(() => section.classList.remove('flash'), 1000);
   });
 });
+//Configurando menu hamburguer
+// cache elements once
+const burger   = document.getElementById('hamburger-menu');
+const listaLink = document.getElementById('nav-links');
+
+// toggle menu
+burger.addEventListener('click', () => {
+  listaLink.classList.toggle('open');     // show / hide links
+  burger.classList.toggle('active');     // swap icons
+});
+
+// close menu when a link is clicked (mobile UX)
+document.querySelectorAll('#nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    listaLink.classList.remove('open');
+    burger.classList.remove('active');
+  });
+});
