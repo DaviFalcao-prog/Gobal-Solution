94767728b2168957fb0872bc948ffa6a089cf1e6
DaviFalcao-prog <davifalcao1609@outlook.com>
2025-06-06 19:48:40 -0300

    Resolvendo navegação entre páginas

--- app.js
+++ app.js
@@ -12,18 +12,20 @@ const allSections = document.querySelectorAll('section');
 
 navLinks.forEach(function (link) {
   link.addEventListener('click', function (e) {
-    e.preventDefault();
 
     const id = this.getAttribute('href'); 
     const section = document.querySelector(id);
     if (!section) return;
 
     //centraliza a seção na tela
+    if(section){
+      e.preventDefault();
     const y =
       section.offsetTop -
       (window.innerHeight / 2 - section.offsetHeight / 2);
 
     window.scrollTo({ top: y});
+  }
 
     //Animação de Destaque das seções, achei que ficou legal então vou deixar comentado aqui.
     //section.classList.add('flash');
